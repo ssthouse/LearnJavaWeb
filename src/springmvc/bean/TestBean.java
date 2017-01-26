6a5f22921cb634129d7f60f75de2a808dde45c95
ssthouse <ssthouse@163.com>
2017-01-26 13:45:00 +0800

    Test SpringMVC return json.

---
+++ src/springmvc/bean/TestBean.java
@@ -0,0 +1,31 @@
+package springmvc.bean;
+
+/**
+ * Created by ssthouse on 26/01/2017.
+ */
+public class TestBean {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public TestBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
--- src/springmvc/controller/HelloController.java
+++ src/springmvc/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springmvc.bean.TestBean;
 
 /**
  * Created by ssthouse on 26/01/2017.
@@ -12,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/hello")
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
-    public String printHello(ModelMap model) {
+    @ResponseBody
+    public TestBean printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
-        return "hello";
+        return new TestBean("ssthouse", 21);
     }
 }
