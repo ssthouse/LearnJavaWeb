5fd542817451aa6bc01afb11caa61840df264846
ssthouse <ssthouse@163.com>
2017-01-26 12:19:41 +0800

    Change jar file location to the web/lib dir, finish springmvc helloworld

---
+++ src/springmvc/controller/HelloController.java
@@ -0,0 +1,19 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by ssthouse on 26/01/2017.
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
---
+++ web/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ssthouse
+  Date: 10/01/2017
+  Time: 3:55 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+</head>
+<body>
+
+hello
+</body>
+</html>
